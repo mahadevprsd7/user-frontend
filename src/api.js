bd647c97d758ab331ff9076d8400409854efbda1
mahadevprsd7 <mahadevprsd7@gmail.com>
2026-05-07 14:29:34 +0530

    Modified API

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://user-backend-vorg.onrender.com/api/users'
+  baseURL: `${import.meta.env.VITE_API_URL}/api/users`
 });
 
 API.interceptors.request.use((req) => {
